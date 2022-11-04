16dee4e061464f904d43b4750eb17003a08ea43e
Genefreak <stevengranville@gmail.com>
2022-11-04 16:48:02 +0000

    adding main code

---
+++ ftype.py
@@ -0,0 +1,28 @@
+# This is a program to take an input from a user
+# and then display it letter for letter with a little sound when each letter is displayed.
+import simpleaudio
+import time
+import os
+
+
+text = input("Hi! Please input your text")
+length = len(text)
+location = 0
+os.system('clear')
+text = text.strip() #delete line breaks
+
+while location < length:
+    wave_obj = simpleaudio.WaveObject.from_wave_file("bell.wav")
+    play_obj = wave_obj.play()
+    #play_obj.wait_done()
+    time.sleep(0.04)
+    print(text[location], end="")
+
+    if text[location] == ",":
+        time.sleep(0.5)
+    if text[location] == ".":
+        time.sleep(1)
+    if text[location] == "/n":
+        print ("ERROR on NEWLINE")
+
+    location += 1
